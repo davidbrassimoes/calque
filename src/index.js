183960faa33c5e106cb45e0c284629ca118e51dd
davidbrassimoes <david.simoes.1994@gmail.com>
2023-11-29 01:17:48 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react"
+import ReactDom from "react-dom/client"
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Main = () => {
+  const [leftOp, setLeftOp] = useState()
+  const [rightOp, setRightOp] = useState()
+  const [operator, setOperator] = useState()
+  const [displayed, setDisplayed] = useState(0)
+  const [isSet, setIsSet] = useState(false)
+  const [result, setResult] = useState()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleClick = (x) => {
+    if (
+      displayed == 0 &&
+      displayed == "0" &&
+      x != "C" &&
+      x != "=" &&
+      x != "/" &&
+      x != "*" &&
+      x != "-" &&
+      x != "+"
+    ) {
+      setDisplayed(x)
+    } else if (x == "C") {
+      setDisplayed(0)
+      setLeftOp()
+      setRightOp()
+      setResult()
+      setOperator()
+      setIsSet(false)
+    } else if (x == "/" || x == "*" || x == "-" || x == "+") {
+      if (isSet) {
+        setResult(calculate(leftOp, displayed, operator))
+        setLeftOp(calculate(leftOp, displayed, operator))
+        setOperator(x)
+        setRightOp()
+        setIsSet(false)
+        setDisplayed(0)
+      } else {
+        setLeftOp(displayed)
+        setOperator(x)
+        setIsSet(true)
+        setDisplayed(0)
+      }
+    } else if (x == "=") {
+      setRightOp(displayed)
+      setResult(calculate(leftOp, displayed, operator))
+      setDisplayed(calculate(leftOp, displayed, operator))
+    } else {
+      setDisplayed("" + displayed + x)
+    }
+  }
+
+  const calculate = (leftOp, rightOp, operator) => {
+    leftOp = parseInt(leftOp)
+    rightOp = parseInt(rightOp)
+    if (operator == "/") {
+      return leftOp / rightOp
+    } else if (operator == "*") {
+      return leftOp * rightOp
+    } else if (operator == "-") {
+      return leftOp - rightOp
+    } else if (operator == "+") {
+      return leftOp + rightOp
+    }
+  }
+
+  return (
+    <>
+      <h1 className='title'>Calque</h1>
+      <Display x={displayed} />
+      <section className='calc'>
+        {input.map((x) => (
+          <button
+            type='button'
+            className='calculatorButton'
+            onClick={() => handleClick(x)}
+            key={x}
+          >
+            {x}
+          </button>
+        ))}
+      </section>
+      <Output
+        leftOp={leftOp}
+        operator={operator}
+        rightOp={rightOp}
+        result={result}
+      />
+    </>
+  )
+}
+
+const Output = ({ leftOp, operator, rightOp, result }) => {
+  return (
+    <div className='output'>
+      <div className='calculating'>
+        {leftOp} {operator} {rightOp}
+      </div>{" "}
+      <div className='result'>{result}</div>
+    </div>
+  )
+}
+
+const Display = ({ x, leftOp, operator, rightOp, result }) => {
+  return <div className='display'>{x} </div>
+}
+
+const input = [7, 8, 9, "*", 4, 5, 6, "/", 1, 2, 3, "-", "C", 0, "=", "+"]
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(<Main />)
